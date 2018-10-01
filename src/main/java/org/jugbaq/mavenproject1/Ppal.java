4e06381c6e716dd61e95dc6edb4eccd799124c35
Javier Rodriguez <rjavier84@gmail.com>
2018-10-01 10:57:25 -0500

    probando mi primer repositorio en github virtual java con Maven

---
+++ src/main/java/org/jugbaq/mavenproject1/Ppal.java
@@ -0,0 +1,18 @@
+/*
+ * Prueba de Aplicacion Java con Maven
+ */
+package org.jugbaq.mavenproject1;
+
+/**
+ *
+ * @author Rjavier84
+ */
+public class Ppal {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Prueba de JavaAplicacion con Maven");
+        
+    }
+    
+}
